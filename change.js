4003ea8d650d17ebc73b5a809a5eb5d3079cf7b8
allkamau <allangithaiga5@gmail.com>
2024-08-26 19:30:12 +0300

    Add change.js that takes key as arg an returns corresponding value from sourceobject and functions to change it

---
+++ change.js
@@ -0,0 +1,17 @@
+// const sourceObject = {
+//     num: 42,
+//     bool: true,
+//     str: 'some text',
+//     log: console.log,
+//   }
+  
+  function get(key) {
+    return sourceObject[key];
+  }
+
+  function set(key, value) {
+    sourceObject[key] = value;
+    return  value;
+  }
+//   console.log(get('num'))
+//   console.log(set('num', 30))
