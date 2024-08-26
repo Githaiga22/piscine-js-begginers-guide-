2209eb92987a05de4517e36f11aca8df36f7cb5a
allkamau <allangithaiga5@gmail.com>
2024-08-26 13:14:03 +0300

    Creat immutable copies of  object using shallow copy and  for  and .

---
+++ mutability.js
@@ -0,0 +1,15 @@
+const person = {
+    name: 'Rick',
+    age: 77,
+    Country: 'US',
+};
+//shallow copy using spread operator
+const clone1 = Object.freeze({...person});
+const clone2 = Object.freeze({...person});
+
+const samePerson = person;
+
+person.age += 1;
+person.Country = 'FR'; 
+
+console.log('clone1:', clone1);
