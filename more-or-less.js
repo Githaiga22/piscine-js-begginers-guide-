cd610b81f6db59c699476ff2f46b39ead58761d6
allkamau <allangithaiga5@gmail.com>
2024-08-26 15:08:35 +0300

    Add more-or-less.js that helps in defining functions for adding and subtracting

---
+++ more-or-less.js
@@ -0,0 +1,9 @@
+const more = (n) => n+1;
+const less = (n) => n-1;
+const add = (n, m) => n+m;
+const sub = (n, m) => n-m;
+
+// console.log(more(10))
+// console.log(less(10))
+// console.log(add(20, 10))
+// console.log(sub(20, 10))
