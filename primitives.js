25a2ac770a714e741b3876bbf1d6b757f44f4619
allkamau <allangithaiga5@gmail.com>
2024-08-26 12:00:49 +0300

    Add file that defines constant variables in js

---
+++ primitives.js
@@ -0,0 +1,8 @@
+//constant variables for strings
+const str = "Allan Kamau";
+//constant variable for number 
+const num = 23;
+//constant varibale for boolean
+const bool = true;
+// constant variable for undefined 
+const undef = undefined;
