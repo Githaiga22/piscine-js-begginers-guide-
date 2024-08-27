86fa5e0e61302956a85138a30a02342d0ed0b3e3
allkamau <allangithaiga5@gmail.com>
2024-08-27 10:29:08 +0300

    Add variable that returns sammales and largest possible number

---
+++ biggie-smalls.js
@@ -0,0 +1,10 @@
+/*Create 2 variables:
+
+    smalls with the smallest possible number value.
+    biggie with the greatest possible number value.
+*/
+var smalls = Number.MIN_VALUE;
+var biggie = Number.MAX_VALUE;
+
+// console.log(smalls)
+// console.log(biggie)
