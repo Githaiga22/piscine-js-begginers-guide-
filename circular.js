e2ea8af3084a28b9b8c130e658f52de29150cd4f
allkamau <allangithaiga5@gmail.com>
2024-08-27 10:12:27 +0300

    Add circular.js that creates an empty object and a property assigned to itself

---
+++ circular.js
@@ -0,0 +1,9 @@
+/*Create an object named circular that has a property
+ named circular with itself as the value.*/
+
+ const circular = {};
+ circular.circular = circular;
+
+// console.log(circular)
+// console.log(circular.circular.circular === circular)
+ 
