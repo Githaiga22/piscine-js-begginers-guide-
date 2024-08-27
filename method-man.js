1a176dc3aced7a80b24ede2c2cecad7eb3def222
allkamau <allangithaiga5@gmail.com>
2024-08-27 12:04:57 +0300

    Add functions for splitting strings,joining arrays,converting to uppercase and lowercase sorrounded by asteriks and return first character to uppercase

---
+++ method-man.js
@@ -0,0 +1,20 @@
+function words(str) {
+    return str.split(' ');
+}
+
+function sentence(arr) {
+    return arr.join(' ');
+}
+
+function yell(str) {
+    return str.toUppercase();
+}
+
+function whisper(str) {
+    return `*${str.toLowerCase()}*`;
+}
+console.log(whisper("Hello World"));
+
+function capitalize(str) {
+    return str[0].t0UpperCase() + str.slice(1).toLowerCase();
+}
