01607b8a85f7e0889c58293dfb4d5ab42b6c06d2
allkamau <allangithaiga5@gmail.com>
2024-08-28 22:12:33 +0300

    Add 3 functions that multiply,divide and return modulo of given 2 input numbers

---
+++ elementary.js
@@ -0,0 +1,13 @@
+function multiply(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    return a / b;
+}
+
+function modulo(a, b) {
+    return a % b
+}
+console.log(multiply(20,10))
+console.log(divide(20,10))
+console.log(modulo(20,10))
