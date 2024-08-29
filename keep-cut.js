8018ccb0c82ab999b2f845e7754dbca0f31673c8
allkamau <allangithaiga5@gmail.com>
2024-08-29 17:30:16 +0300

    add keep-cut.js

---
+++ keep-cut.js
@@ -0,0 +1,53 @@
+function cutFirst(str) {
+    if (typeof str !== 'string') {
+        throw new TypeError('argument must be a string');
+    }
+    return str.slice(2);
+}
+
+console.log(cutFirst("hello"))
+
+function cutLast(str) {
+    if (typeof str !== 'string') {
+        throw new TypeError('argument must be a string');
+    }
+    return str.slice(0, -2);
+}
+
+console.log(cutLast("hello"))
+
+function cutFirstLast(str) {
+    if (typeof str !== 'string') {
+        throw new TypeError('argument must be a string');
+    }
+    return str.slice(2, -2);
+}
+
+console.log(cutFirstLast("hello"))
+
+function keepFirst(str) {
+    if (typeof str !== 'string') {
+        throw new TypeError('argument must be a string');
+    }
+    return str.slice(0, 2);
+}
+
+console.log(keepFirst("hello"))
+
+function keepLast(str) {
+    if (typeof str !== 'string') {
+        throw new TypeError('argument must be a string');
+    }
+    return str.slice(-2);
+}
+
+console.log(keepLast("hello"))
+
+function keepFirstLast(str) {
+    if (typeof str !== 'string') {
+        throw new TypeError('argument must be a string');
+    }
+    return (keepFirst(str) + keepLast(str));
+}
+
+console.log(keepFirstLast("hello"))
