ce88ece82f9645f970314b60b626b7d8bc730eb3
allkamau <allangithaiga5@gmail.com>
2024-08-31 02:23:00 +0300

    fix: triangle.js

--- triangle.js
+++ triangle.js
@@ -1,6 +1,6 @@
 function triangle( char, height) {
     let result = "";
-    for (let i = 0; i < height; i++) {
+    for (let i = 0; i <= height; i++) {
         result +=  char.repeat(i) + '\n';
         }
         return result;
