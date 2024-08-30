60c9533706501c4df4a2672db7baf87989963e32
allkamau <allangithaiga5@gmail.com>
2024-08-30 16:43:52 +0300

    fix function that truncates

--- cut-corners.js
+++ cut-corners.js
@@ -18,10 +18,4 @@ function floor(num) {
     return num < intPart ? intPart - 1 : intPart; // If num is less than intPart, decrement
 }
 
-// Test array
-const nums = [3.7, -3.7, 3.1, -3.1];
 
-console.log(nums.map(round)); // Output: [ 4, -3, 3, -3 ]
-console.log(nums.map(floor)); // Output: [ 3, -4, 3, -4 ]
-console.log(nums.map(trunc)); // Output: [ 3, -3, 3, -3 ]
-console.log(nums.map(ceil));  // Output: [ 4, -3, 4, -3 ]
