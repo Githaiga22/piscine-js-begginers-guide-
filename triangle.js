490ee30abd575d4f062cc41f8df73fbaaf2b2457
allkamau <allangithaiga5@gmail.com>
2024-08-31 02:20:21 +0300

    Add triangle.js

---
+++ triangle.js
@@ -0,0 +1,8 @@
+function triangle( char, height) {
+    let result = "";
+    for (let i = 0; i < height; i++) {
+        result +=  char.repeat(i) + '\n';
+        }
+        return result;
+}
+console.log(triangle('*', 5));
