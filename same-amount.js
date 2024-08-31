7804e97248bd2430ccbf0b1ce9fc09fb16bbe057
allkamau <allangithaiga5@gmail.com>
2024-08-31 19:02:52 +0300

    Fix: same-amaount.js

--- same-amount.js
+++ same-amount.js
@@ -4,12 +4,17 @@ function sameAmount(str, regex1, regex2) {
     let matches1 = str.match(regex1);
     let matches2 = str.match(regex2);
 
+    if (matches1 === null || matches2 === null){
+        return false;
+    }
+
     let count1 = matches1 ? matches1.length : 0;
     let count2 = matches2 ? matches2.length : 0;
 
     return count1 === count2
 }
 
-console.log(sameAmount("aabb", /a/g, /b/g)); // true
-console.log(sameAmount("aabbcc", /a/g, /b/g)); // true
-console.log(sameAmount("aabbcc", /a/g, /c/g)); // false
+console.log(sameAmount("aabb", /a/g, /b/g)); 
+console.log(sameAmount("aabbcc", /a/g, /b/g)); 
+console.log(sameAmount("aabbcc", /a/g, /c/g)); 
+console.log(sameAmount("data", /q /, /qqqqqqq/)); 
