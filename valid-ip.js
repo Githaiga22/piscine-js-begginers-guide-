f5db00fac1e91360f3425a238a55b9b463df047f
Allan  Kamau <allkamau@noreply.learn.zone01kisumu.ke>
2024-08-31 17:41:38 +0000

    Fix code

--- valid-ip.js
+++ valid-ip.js
@@ -3,13 +3,11 @@ function findIP(input) {
     const ipRegex = /\b((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])\.){3}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])(?::([0-9]{1,5}))?\b/g;
     let matches;
     const result = [];
-
     while ((matches = ipRegex.exec(input)) !== null) {
-        const port = matches[4];
-        if (port === undefined || (port >= 0 && port <= 65535)) {
+        const port = matches[5]; // Change to the correct index
+        if (port === undefined || (parseInt(port) >= 0 && parseInt(port) <= 65535)) {
             result.push(matches[0]);
         }
     }
-
     return result;
 }
