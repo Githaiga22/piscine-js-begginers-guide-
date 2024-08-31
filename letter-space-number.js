9a041d768dcda3472cc45ee1046bd11f327fa5c3
allkamau <allangithaiga5@gmail.com>
2024-08-31 17:52:46 +0300

    Fix: regular expression looking for a letter followed by a digit then a space

--- letter-space-number.js
+++ letter-space-number.js
@@ -1,7 +1,11 @@
 function letterSpaceNumber(str) {
     let result = []
-    let Regex = /[a-zA-Z]\d /g;
-     result = str.match(Regex)
+    let Regex = /[a-zA-Z] \d(?![a-zA-Z0-9])/g;
+    let matched = str.match(Regex)
+    if (matched){
+        result = matched;
+    }
     return result
 }
 console.log(letterSpaceNumber("Allan9 hello"))
+console.log(letterSpaceNumber('example 1, example 20'))
