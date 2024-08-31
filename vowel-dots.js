f80f2503b5fc99af10b9b70384297ae9bd717575
allkamau <allangithaiga5@gmail.com>
2024-08-31 18:21:25 +0300

    Fix:  function that returns a (.) in every instance of a vowel

--- vowel-dots.js
+++ vowel-dots.js
@@ -1,13 +1,6 @@
 function vowelDots(str) {
-    let vowels = "aeiou";
-    let result = "";
-    for (let i = 0; i < str.length; i++) {
-        if (vowels.includes(str[i].toLowerCase())) {
-            result += str[i] + ".";
-            } else {
-                result += str[i];
-                }
-                }
-                return result;
+    let vowels = /[aeiouAEIOU]/g;
+    let result = str.replace(vowels, "$&.");
+  return result;
 }
-console.log(vowelDots("Hello World")); // "He.llo Wo.rld"``
+console.log(vowelDots("Allan")); 
