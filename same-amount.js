c3aa0cae8529b049d0b5d585d9f9e2a776653a41
allkamau <allangithaiga5@gmail.com>
2024-08-31 19:06:15 +0300

    Fix: same-amaount.js

--- same-amount.js
+++ same-amount.js
@@ -1,8 +1,8 @@
 
 function sameAmount(str, regex1, regex2) {
 
-    let matches1 = str.match(regex1);
-    let matches2 = str.match(regex2);
+    let matches1 = str.match(new RegExp(regex1,"g"));
+    let matches2 = str.match(new RegExp(regex2,"g"));
 
     if (matches1 === null || matches2 === null){
         return false;
