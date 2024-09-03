1690e17bbdc06e500cb4b98e8984d52f369b2488
allkamau <allangithaiga5@gmail.com>
2024-09-03 14:26:22 +0300

    Debug code:  function that compares valid dates, after and before dates and future and past dates

--- date-is.js
+++ date-is.js
@@ -27,8 +27,22 @@ function isAfter(date1, date2) {
 console.log(isAfter(new Date('2023-08-29'), new Date('2023-08-28'))); // true
 console.log(isAfter(new Date('2023-08-27'), new Date('2023-08-28'))); // false
 
-function isBefore(date1, date2){
-    return isValid(date1) && isValid(date2) && date1.getTime() < date2.getTime();
+function isBefore(date1, date2) {
+    // If either argument is not provided or not a valid type, return false
+    if (date1 == null || date2 == null || (typeof date1 !== 'number' && !(date1 instanceof Date)) || (typeof date2 !== 'number' && !(date2 instanceof Date))) {
+        return false;
+    }
+
+    // Convert timestamps (numbers) to Date objects if necessary
+    if (typeof date1 === 'number') {
+        date1 = new Date(date1);
+    }
+    if (typeof date2 === 'number') {
+        date2 = new Date(date2);
+    }
+
+    // Return true if the first date is before the second date
+    return date1.getTime() < date2.getTime();
 }
 console.log(isBefore(new Date('2023-08-29'), new Date('2023-08-28'))); // true
 console.log(isBefore(new Date('2023-08-27'), new Date('2023-08-28'))); // false
