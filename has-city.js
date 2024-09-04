a6dbfb42ebe8ec94148277480fd832d43828b4c5
allkamau <allangithaiga5@gmail.com>
2024-09-05 01:06:06 +0300

    Add hascity.js

--- has-city.js
+++ has-city.js
@@ -1,8 +1,8 @@
 function hasCity(country, cities) {
     return function (city) {
-    if (cities.indexOf(city) === -1) {
-        return city + " is not a city from" + country;
-    }
-    return city + " is a city from " + country;
-};
+        if (cities.indexOf(city) === -1) {
+            return city + " is not a city from " + country;
+        }
+        return city + " is a city from " + country;
+    };
 }
