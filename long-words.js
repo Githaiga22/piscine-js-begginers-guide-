94f871318368690c9c4eb017f156a36cac85e408
allkamau <allangithaiga5@gmail.com>
2024-09-04 21:03:17 +0300

    Add longword.js

--- long-words.js
+++ long-words.js
@@ -1,7 +1,7 @@
-function longwords(array) {
+function longWords(array) {
     return array.every(word => typeof word === 'string' && word.length >= 5);
 }
-console.log(longwords(["Today is a monday"]))
+console.log(longWords(["Today is a monday"]))
 
 function oneLongWord(arr) {
     return arr.some(word => typeof word === 'string' && word.length >= 10);
