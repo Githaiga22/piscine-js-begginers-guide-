2d2081fff80aefbca2d9a2067e42f319d719fbe5
allkamau <allangithaiga5@gmail.com>
2024-09-04 21:22:48 +0300

    Add longword.js

--- long-words.js
+++ long-words.js
@@ -1,13 +1,13 @@
 function longWords(array) {
-    return array.every(word => typeof word === 'string' && word.length >= 5);
+    return array.every((word) =>  word.length >= 5);
 }
 console.log(longWords(["Today is a monday"]))
 
 function oneLongWord(arr) {
-    return arr.some(word => typeof word === 'string' && word.length >= 10);
+    return arr.some((word) =>  word.length >= 10);
 }
 console.log(oneLongWord(["Hello Monday", "world", "how", "are", "you"]))
 
 function noLongWords(array) {
-    return array.every(word => typeof word !== 'string' && word.length < 7);
+    return array.every((word) => word.length < 7);
 }
