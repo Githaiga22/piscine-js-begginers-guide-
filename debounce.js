5497a8c11b9390c41819f8f955d320b95fc5eb96
allkamau <allangithaiga5@gmail.com>
2024-09-13 16:08:55 +0300

    Fix code

--- debounce.js
+++ debounce.js
@@ -1,36 +1,19 @@
-function debounce(fn, delay) {
-    let timer = null;
-    
-    return function (...args) {
-        clearTimeout(timer);
-        timer = setTimeout(() => {
-            fn.apply(this, args);
-        }, delay);
-    };
+"use strict";
+
+function debounce(func, wait = 0) {
+    let timer
+    return (...args) => {
+        clearTimeout(timer)
+        timer = setTimeout(() => func(...args), wait)
+    }
 }
-function opDebounce(fn, delay, options = {}) {
-    let timer = null;
-    let leadingCalled = false;
-    const { leading = false } = options;
 
-    return function (...args) {
-        const context = this;
 
-        if (leading && !leadingCalled) {
-            // Call immediately on the leading edge if `leading` is true
-            fn.apply(context, args);
-            leadingCalled = true;
-        }
-
-        // Clear the previous timer
-        if (timer) {
-            clearTimeout(timer);
-        }
-
-        // Set a new timer
-        timer = setTimeout(() => {
-            fn.apply(context, args);
-            leadingCalled = false; // Allow leading edge call in the future
-        }, delay);
-    };
+function opDebounce(func, wait = 0, options = {}) {
+    let timer
+    return (...args) => {
+        if (!timer && options["leading"]) func(...args)
+        clearTimeout(timer)
+        timer = setTimeout(() => func(...args), wait)
+    }
 }
