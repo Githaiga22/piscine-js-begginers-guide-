4dcd50158738785ca410c3888a08f08bf8fddb91
allkamau <allangithaiga5@gmail.com>
2024-09-14 10:55:53 +0300

    Add interpolation function with callback for step-based value interpolation

---
+++ interpolation.js
@@ -0,0 +1,12 @@
+"use strict";
+
+function interpolation(obj) {
+    const {step, start, end, callback, duration} = obj
+    let i = 0
+    const interval = duration/step
+    let timer = setInterval(() => {
+        callback([((end-start)/step*i)+start, interval * (i+1)])
+        i++
+        if (i === step) clearInterval(timer)
+    }, interval)
+}
