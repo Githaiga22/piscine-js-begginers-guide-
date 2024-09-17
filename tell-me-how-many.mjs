65b45e3b125b0b96cfd5c800c37d6a5a0e3383be
allkamau <allangithaiga5@gmail.com>
2024-09-17 17:23:12 +0300

    tell-me-how-many.mjs

---
+++ tell-me-how-many.mjs
@@ -0,0 +1,22 @@
+import { promises as fs } from 'fs';
+import { resolve } from 'path';
+
+
+//function to read the directory and count its entries
+
+async function countDirectoryEntries(directory) {
+    try {
+        //read directories contents
+        const entries = await fs.readdir(directory)
+        //count and log the nummber of entries
+        console.log('number of entries in the directory:', entries.length);
+    } catch (err) {
+        //handle errors eg if the directory does not exist
+        console.error('Error reading directory:', err.message)
+    }
+}
+
+//get the directory path from cmd
+const directory = process.argv[2] ? resolve(process.argv[2]) : process.cwd()
+
+countDirectoryEntries(directory)
