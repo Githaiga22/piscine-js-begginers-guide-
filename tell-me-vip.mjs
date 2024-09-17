1671bf886620210b2bcc754e4207b369653dd787
allkamau <allangithaiga5@gmail.com>
2024-09-17 18:59:30 +0300

    tell-me-vip.mjs

---
+++ tell-me-vip.mjs
@@ -0,0 +1,47 @@
+import { promises as fs } from 'fs'
+import path from 'path'
+
+async function getVip(inputPath) {
+  try {
+    const files = await fs.readdir(inputPath)
+    const guests = []
+
+    for (const file of files) {
+      const filePath = path.join(inputPath, file)
+      const content = JSON.parse(await fs.readFile(filePath, 'utf8'))
+
+      if (content.answer.toLowerCase() === 'yes') {
+        // Extract the base name of the file without extension
+        const baseName = path.basename(file, '.json')
+
+        // Assuming the base name is the guest's name, use it directly
+        guests.push(baseName)
+      }
+    }
+
+    guests.sort()
+
+    const formattedGuests = guests.map(
+      (guest, index) => `${index + 1}. ${guest}`,
+    )
+
+    const vipList = formattedGuests.join('\n')
+    await fs.writeFile('vip.txt', vipList, 'utf8')
+    return vipList
+  } catch (error) {
+    console.error('Error processing the guest files:', error.message)
+  }
+}
+
+const inputPath = process.argv[2]
+
+if (!inputPath) {
+  console.error('No directory path provided!')
+  process.exit(1)
+}
+
+getVip(inputPath).then((result) => {
+  if (result) {
+    console.log(result)
+  }
+})
